74f7a724d6f95c5ebe76e9b80f3deb1169a161b0
Larissa Ribeiro de Souza <souzalarissa383@gmail.com>
2022-09-18 23:02:18 -0300

    Delete Problema (Builder)/FactoryMethod directory

--- Problema (Builder)/FactoryMethod/EmissorEmail.java
+++
@@ -1,6 +0,0 @@
-public class EmissorEmail implements Emissor {
-  public void envia(String message){
-    System.out.println("Enviando por E-Mail a mensagem: ");
-    System.out.println(message);
-  }
-}
--- Problema (Builder)/FactoryMethod/EmissorJMS.java
+++
@@ -1,6 +0,0 @@
-public class EmissorJMS implements Emissor {
-  public void envia(String message){
-    System.out.println("Enviando por JMS a mensagem: ");
-    System.out.println(message);
-  }
-}
