dfa20d8b00add3869592b7f53f2cc227b9ddee4b
Larissa Ribeiro de Souza <souzalarissa383@gmail.com>
2022-09-18 22:53:14 -0300

    Create EmissorSMS.java

---
+++ Problema (Builder)/FactoryMethod/EmissorSMS.java
@@ -0,0 +1,6 @@
+public class EmissorSMS implements Emissor {
+  public void envia(String message){
+    System.out.println("Enviando por SMS a mensagem: ");
+    System.out.println(message);
+  }
+}
