db77b6e9a190562104d5385513743e4bf747a485
Larissa Ribeiro de Souza <souzalarissa383@gmail.com>
2022-09-18 22:46:08 -0300

    Delete TesteSingleton.java

--- TesteSingleton.java
+++
@@ -1,7 +0,0 @@
-public class TesteSingleton {
-    public static void main(String[] args) {
-      Configuracao configuracao = Configuracao.getInstance();
-      System.out.println(configucao.getPropriedade("time-zone"));
-      System.out.println(configucao.getPropriedade("currency-code"));
-    }
-}
