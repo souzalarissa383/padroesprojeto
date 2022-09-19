12222fad636c555106762ca5d3aca7df2886f95d
Larissa Ribeiro de Souza <souzalarissa383@gmail.com>
2022-09-18 22:55:18 -0300

    Create EmissorJMS.java

---
+++ Problema (Builder)/FactoryMethod/EmissorJMS.java
@@ -0,0 +1,6 @@
+public class EmissorJMS implements Emissor {
+  public void envia(String message){
+    System.out.println("Enviando por JMS a mensagem: ");
+    System.out.println(message);
+  }
+}
